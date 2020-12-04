a1e94278cccc4dd71ba6eb839cc4eafd6b2315ed
TynClause <elvinnurfurqon17@gmail.com>
2020-12-04 18:53:03 +0700

    update tentang kami screen

--- src/pages/tentangKamiScreen.js
+++ src/pages/tentangKamiScreen.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView,ScrollView,View,Text,Image,StyleSheet } from 'react-native';
+import { Logo } from '../assets';
+
+const textPesan = "Nakama merupakan sebuah aplikasi jual beli hasi pertanian yang memungkinkan semua orang dapat menjual ataupun membeli tanpa perlu repot mencarinya, dasar terciptanya aplikasi ini karena masih banyaknya petani kecil yang kesulitan dalam memasarkan hasil pertaniannya.";
 
 
 const TentangKami = () => {
     return (
-        <View>
-            Tentang Kami
-        </View>
+        <SafeAreaView>
+            <ScrollView>
+                <Image source={Logo}/>
+                <Text>{textPesan}</Text>
+            </ScrollView>
+        </SafeAreaView>
     );
-
 }
 
 export default TentangKami;
+
+const style = StyleSheet.create({
+    imageStyle:{},
+    textStyle:{},
+    });
